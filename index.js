5c55af04cdd720043e94fcc731b45638c40faf1f
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2023-11-09 23:53:07 +0400

    Function Task

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 function Task (id, description, cost){
-  const id = this.id;
-  const description = this.description;
-  const cost = this.cost;
+  if(!(this instanceof Task)) {
+    throw new Error ("Функция должна быть вызвана при помощи 'New'")
+  }
 
-  
+  const _id = id;
+  const _description = description;
+  let _cost = cost;
+
+  Object.defineProperty(this, 'id', {
+    get: function() {
+      return _id;
+    }
+  });
+
+  Object.defineProperty(this, 'description', {
+    get: function() {
+      return _description;
+    }
+  });
+
+  Object.defineProperty(this, 'cost', {
+    get: function() {
+      return _cost;
+    }
+  });
+
+  if(_cost < 0){
+    throw new Error('Стоимость должна быть 0')
+  }
 }
+
