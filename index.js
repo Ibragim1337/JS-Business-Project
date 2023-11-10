183374c4d67ac22f7a10fbe0b28cfb6089807f0f
Алакпаров Ибрагим Эхтият <Alakp_mr11@itstep.edu.az>
2023-11-10 14:14:21 +0400

    Added new classe and errors in task

--- index.js
+++ index.js
@@ -3,30 +3,65 @@ function Task (id, description, cost){
     throw new Error ("Функция должна быть вызвана при помощи 'New'")
   }
 
-  const _id = id;
-  const _description = description;
-  let _cost = cost;
+  this._id = "id" + Math.random().toString(16).slice(2);
+  if (typeof description === "string") {
+    this._description = description;
+  } else {
+    throw new Error ("Значение должно быть строкой")
+  }
+ 
+  if (typeof cost === "number" && cost >= 0 ){
+    this._cost = cost;
+  } else {
+    throw new Error ("Значение должно быть число и больше 0")
+  }
+    // this._description = description;
+  // this._cost = cost;
 
   Object.defineProperty(this, 'id', {
     get: function() {
-      return _id;
+      return id;
     }
   });
 
   Object.defineProperty(this, 'description', {
     get: function() {
-      return _description;
+      return description;
     }
   });
 
   Object.defineProperty(this, 'cost', {
     get: function() {
-      return _cost;
+      return cost;
     }
   });
 
-  if(_cost < 0){
+  if(cost < 0){
     throw new Error('Стоимость должна быть 0')
   }
 }
 
+
+class IncomeTask extends Task {
+  constructor(id, description, cost){
+    super (id, description, cost);
+  }
+}
+
+class ExspenseTask extends Task {
+  constructor(id, description, cost){
+    super (id, description, cost);
+  }
+  
+}
+
+class TaskController {
+  #tasks;
+  constructor (tasks) {
+    this.#tasks;
+  }
+}
+
+const task1 = new Task('', 'task1 money', 3000);
+
+console.log(task1.description);
